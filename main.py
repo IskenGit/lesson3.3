4985a4b9e9781e465d787aede9acada35a1f19cd
IskenGit <isken1990@gmail.com>
2024-06-04 14:57:06 +0600

    add all const

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("img/RSK24.png")
+icon = pygame.image.load("img/5656.jpg")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/")
+target_img = pygame.image.load("img/target.png")
 target_width = 30
 target_height = 50
 
